7c663c3eeb60cd165c59f26f5b889a261fd28874
F <fmalina@gmail.com>
2015-05-11 19:57:45 +0100

    Fix loading of Unilexicon SKOS vocabularies. Add private vocab option that stays hidden for now.

---
+++ apps/vocabulary/migrations/0002_auto_20150511_1942.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('vocabulary', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='vocabulary',
+            name='private',
+            field=models.BooleanField(verbose_name='Private vocabulary', default=False, help_text='Private vocabulary can be edited only by the users belonging to its authority.'),
+        ),
+        migrations.AlterField(
+            model_name='vocabulary',
+            name='description',
+            field=models.TextField(null=True, max_length=200, blank=True),
+        ),
+    ]
