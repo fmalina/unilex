f7e90b9446aedccabcb7fb00f2fa5b0e1730b178
F <fmalina@gmail.com>
2015-05-14 22:21:15 +0100

    Update nothing to tag.

--- apps/tag/urls.py
+++ apps/tag/urls.py
@@ -3,6 +3,7 @@ from django.conf import settings
 
 urlpatterns = patterns('tag.views',
     (r'^$', 'nothing'),
+    (r'^false$', 'nothing'),
     (r'^about$', 'about'),
     (r'^(?P<node_id>\d+)', 'tag'),
     (r'^query$', 'query'),
