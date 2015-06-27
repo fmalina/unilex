e6b1c6a8d56f13b3a8cdcfcfef61fe163fc5e423
F <fmalina@gmail.com>
2015-06-28 00:48:55 +0100

    Spreadsheet import - allow to upload and process CSV spreadsheets, not just Excel - more rigorous tests - better error handling

--- apps/vocabulary/test_load_xls.py
+++ apps/vocabulary/test_load_xls.py
@@ -3,6 +3,7 @@ from django.test.client import RequestFactory
 from django.contrib.auth.models import User
 from django.contrib.messages.storage.fallback import FallbackStorage
 from vocabulary.load_xls import load_xls
+from vocabulary.models import Vocabulary
 
 class LoadTestCase(TestCase):
     def test_loading(self):
@@ -13,12 +14,17 @@ class LoadTestCase(TestCase):
         request.session='session'
         request._messages = FallbackStorage(request)
         test_files = [
-            'taxonomy-with-ids.en-GB.xls',
-            'regions-no-ids.xls'
+            (5427, 'taxonomy-with-ids.en-GB.xls'),
+            (28, 'regions-no-ids1.xls'),
+            (28, 'regions-no-ids.csv'),
+            (28, 'regions-with-ids.csv'),
+            (28, 'regions-with-ids-parents.csv')
         ]
         
-        for fn in test_files:
+        for no, fn in test_files:
             with open('vocabulary/test_data/'+fn, 'rb') as f:
                 slug = fn.split('.')[0]
-                url = load_xls(request, f)
+                url = load_xls(request, f.read(), slug)
                 self.assertEqual(url, '/vocabularies/'+slug+'/')
+                v = Vocabulary.objects.all().last()
+                self.assertEqual(no, v.concept_set.all().count())
