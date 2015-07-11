a18cdc823630eb58abfb230f9bff99fdcc12ee7a
F <fmalina@gmail.com>
2015-07-11 13:55:24 +0100

    Amend

--- tagbuild.py
+++ tagbuild.py
@@ -2,6 +2,8 @@
 import shutil
 
 for asset in 'autocomplete jquery.formset vocab_set'.split():
+    print('Copying %s.js' % asset)
     shutil.copy('static/js/%s.js' % asset, 'tag/js.build/')
 
+print('Packing tag.zip')
 shutil.make_archive('tag', 'zip', 'tag')
