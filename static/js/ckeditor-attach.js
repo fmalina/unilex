6f160f4941775a4ffbe1bfe772665b61b96460c0
F <fmalina@gmail.com>
2015-07-14 17:49:43 +0100

    Remove CKEditor.

--- static/js/ckeditor-attach.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function() {
-    attach_ckeditor('id_body');
-    $('label[for="id_body"]').hide();
-});
--- static/js/ckeditor-customize.js
+++
@@ -1,7 +0,0 @@
-function attach_ckeditor(attach_to){
-    CKEDITOR.replace(attach_to,{
-		toolbar: [
-			[ 'Undo','Redo','-','Bold','Italic','Link','Unlink','Subscript','Superscript','Strike','-','BulletedList','NumberedList','Blockquote','Image','-','Format','-', 'Source'],
-		]
-	});
-}
