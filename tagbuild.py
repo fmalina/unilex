1c2cf7e678e1f809e4d5ce07bb2dece62f3257e8
F. Malina <fmalina@pm.me>
2015-07-22 22:40:41 +0100

    Update autocomplete.

--- tagbuild.py
+++ tagbuild.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import shutil
 
-for asset in 'autocomplete jquery.formset vocab_set'.split():
+for asset in 'jquery.autocomplete jquery.formset vocab_set'.split():
     print('Copying %s.js' % asset)
     shutil.copy('static/js/%s.js' % asset, 'tag/js.build/')
 
