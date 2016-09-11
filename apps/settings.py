a9202204f74a7e6a82c91caf70b1ccb37f8d9640
F <fmalina@gmail.com>
2016-09-11 13:30:51 +0100

    Updated settings TEMPLATES to new format.

--- apps/settings.py
+++ apps/settings.py
@@ -23,11 +23,32 @@ LOGIN_REDIRECT_URL = '/'
 from django.utils.log import DEFAULT_LOGGING as LOGGING
 LOGGING['handlers']['mail_admins']['include_html'] = True
 
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-)
-
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            PWD + '/templates/',
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'vocabulary.context_processors.current_site',
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.contrib.messages.context_processors.messages',
+                'django.core.context_processors.request',
+            ],
+            'loaders': (
+                'django.template.loaders.filesystem.Loader',
+                'django.template.loaders.app_directories.Loader',
+            )
+        },
+    },
+]
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -38,10 +59,6 @@ MIDDLEWARE_CLASSES = (
 )
 
 ROOT_URLCONF = 'urls'
-
-TEMPLATE_DIRS = [
-   PWD + '/templates/',
-]
 
 INSTALLED_APPS = (
     'django.contrib.auth',
@@ -56,13 +73,3 @@ INSTALLED_APPS = (
     'registration',
     'feedback',
 )
-
-TEMPLATE_CONTEXT_PROCESSORS = [
-    "vocabulary.context_processors.current_site",
-    "django.contrib.auth.context_processors.auth",
-    "django.core.context_processors.debug",
-    "django.core.context_processors.i18n",
-    "django.core.context_processors.media",
-    "django.contrib.messages.context_processors.messages",
-    "django.core.context_processors.request",
-]
