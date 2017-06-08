ac4ef8393cd2a1a59cfa5dfae6752544f368283f
Frantisek Malina <frantisek.malina@interoute.com>
2017-06-08 21:57:44 +0100

    Make vocabulary private by default.

--- apps/vocabulary/models.py
+++ apps/vocabulary/models.py
@@ -43,7 +43,7 @@ class Vocabulary(models.Model):
     language    = models.ForeignKey(Language, blank=True, null=True)
     authority   = models.ForeignKey(Authority, blank=True, null=True)
     queries     = models.BooleanField(verbose_name="Enable queries?", default=False)
-    private     = models.BooleanField(verbose_name="Private vocabulary", default=False,
+    private     = models.BooleanField(verbose_name="Private vocabulary", default=True,
         help_text="Private vocabulary can be edited only by the users belonging to its authority.")
     source      = models.URLField(blank=True)
     updated_at  = models.DateTimeField(default=datetime.now, editable=False)
