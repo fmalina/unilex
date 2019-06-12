6b5a1920388275fe6d5bd4e6106976ea9a710bab
Francis Malina <francis@vizualbod.com>
2019-06-12 16:06:53 +0100

    Added a tagging prototype

--- apps/tag/views.py
+++ apps/tag/views.py
@@ -24,39 +24,53 @@ class TaggingView(View):
     remote = False
 
     @method_decorator(csrf_exempt)
-    def post(self, request, url):
-        record, created = Record.objects.get_or_create(url=url)
+    def post(self, request, url=None):
+        record = None
+        if url:
+            record, _created = Record.objects.get_or_create(url=url)
+
         form = RecordForm(request.POST, instance=record)
         tag_formset = formset_factory(form=TagForm)
         formset = tag_formset(request.POST)
 
+        tags = []
+        for tag_form in formset.forms:
+            raw_tag = tag_form['to_concept'].data
+            if raw_tag:
+                tag = Concept.objects.get(pk=raw_tag)
+                tags.append(tag)
         if not form.is_valid():
             print(form.errors)
         else:
             d = form.cleaned_data
-            tags = []
-            for tag_form in formset.forms:
-                raw_tag = tag_form['to_concept'].data
-                if raw_tag:
-                    tag = Concept.objects.get(pk=raw_tag)
-                    tags.append(tag)
             if self.remote:
-                print('remote')
                 response = HttpResponse(Client.post_tags(d, tags, d['auth_token']))
             else:
-                record.title = d['title']
+                if record:
+                    record.title = d['title']
+                else:
+                    record = Record(**d)
                 record.save()
                 for tag in tags:
                     print(tag)
                     _tag, _created = Tag.objects.get_or_create(record=record, concept=tag)
                 response = redirect(record.get_absolute_url())
             return response
+        return render(request, self.template_name, {
+            'record': record,
+            'form': form,
+            'formset': formset,
+            'forms_and_set': zip(formset.forms, tags)
+        })
 
-    def get(self, request, url):
+
+    def get(self, request, url=None):
         tag_concepts = []
         tag_forms = []
+        concepts = []
+        record = None
 
-        if self.remote:  # remote source of tagging data
+        if self.remote and url:  # remote source of tagging data
             query = url
             try:
                 data = Client(request, query).get_tags()
@@ -67,15 +81,14 @@ class TaggingView(View):
                 return HttpResponse(msg + traceback.format_exc(), content_type='text/plain')
 
             record = Record.objects.get(id=json_record['id'])
-            concepts = []
             for t in json_record['tags']:
                 t = t.split()
                 vocab = Vocabulary.objects.get(node_id=t[0])
                 concept = Concept.objects.get(vocabulary=vocab, node_id=t[1])
                 concepts.append(concept)
-        else:
+        elif url:
             # local tag repository
-            record, created = Record.objects.get_or_create(url=url)
+            record, _created = Record.objects.get_or_create(url=url)
             concepts = [tag.concept for tag in record.tag_set.all()]
 
         for concept in concepts:
@@ -95,7 +108,7 @@ class TaggingView(View):
             'record': record,
             'form': form,
             'formset': formset,
-            'forms_and_set': forms_and_tags
+            'forms_and_set': zip(formset.forms, tag_concepts)
         })
         # CORS header
         response['Access-Control-Allow-Origin'] = '*'
@@ -119,7 +132,7 @@ def query(request):
 
 def records(request):
     return render(request, 'tag/records.html', {
-        'title': 'Recently tagged resources',
+        'title': 'Recent records',
         'records': Record.objects.prefetch_related(
             'tag_set__concept',
             'tag_set__concept__vocabulary'
