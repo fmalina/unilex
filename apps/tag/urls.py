ccea733110ad1a12786b590ca07a9d8c8d0c1ccd
F. Malina <fmalina@pm.me>
2019-06-12 13:01:36 +0100

    Added simple tagging prototype

--- apps/tag/urls.py
+++ apps/tag/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from tag.views import about, tag, records, record_json, query
+from tag.views import TaggingView, about, records, record_json, query
 
 urlpatterns = [
     path('about', about, name="about"),
-    path('<int:node_id>', tag, name="tag"),
     path('records', records, name="records"),
     path('json/<int:record_id>', record_json, name="record_json"),
     path('query', query, name="query"),
+    path('-<path:url>', TaggingView.as_view(), name="tag-record"),
+    path('<path:url>', TaggingView.as_view(template_name='tag/record.html'), name="tag"),
 ]
--- tag/tag.js
+++ tag/tag.js
@@ -1,115 +1,55 @@
 var tag = {
     // configure this
-    repository: 'https://unilexicon.com', // vocabulary repository url, has to be https
-    // done configuring
-    site: function(){
-        return window.localStorage.getItem('site');
-    }, // deployment domain
-    id: function(url){
-        // catch the id from paths such as
-        // node/id or node/id/somethingelse
-        var id = false;
-        var parts = url.split('/');
-        var br = false;
-        for(x=0; x<parts.length; x++){
-            if(br){id = parts[x];}
-            if(parts[x]=='node'){br = true;}
-        }
-        return id;
-    },
-    
+    repository: 'https://unilexicon.co',//m', // vocabulary repository url, has to be https
+
     isAllowed: function(tabId, changeInfo, tab) {
         // Called when the url of a tab changes.
         // If the site string is found in the tab's URL, show page action
-        if(tab.url.indexOf(tag.site()) > -1 && tag.id(tab.url)){
+        if(tab.url){
             chrome.pageAction.show(tabId);
-            window.localStorage.removeItem(tag.id(tab.url));
+            window.localStorage.removeItem(tab.url);
         }
-    },
-    
-    authToken: function(){
-        // Create SWORD authentication token in agreed format
-        var password = window.localStorage.getItem('password');
-        var username = window.localStorage.getItem('username');
-        password = md5(password);
-        token = 'username=' + username + ':password=' + password;
-        return md5(token);
-    },
-    
-    setAuth: function(){
-        $('#id_name').val(window.localStorage.getItem('name'));
-        $('#id_auth_token').val(tag.authToken());
-    },
-    
-    save: function(id){
-        window.localStorage.setItem(id, $('body').html());
-        window.localStorage.setItem(id+'title',       $('#id_title').val());
-        window.localStorage.setItem(id+'notes',       $('#id_notes').val());
-        window.localStorage.setItem(id+'description', $('#id_description').val());
     },
 
     saveCurrent: function(){
         chrome.tabs.getSelected(null, function(tab){
-            tag.save(tag.id(tab.url));
+            // custom save action for localStorage
         });
     },
-    
-    tagging: function(id){
+
+    tagging: function(tab){
         var body = $('body');
-        body.html('Loading...');
-        var state = window.localStorage.getItem(id);
-    
-        if(state == undefined){
-            console.log(tag.repository);
-            body.load(tag.repository + '/tag/'+id, function(){
-                // save state
-                tag.save(id);
-                tag.setAuth();
+        if(body.text().indexOf('Loading...') >= 0){
+            body.load(tag.repository + '/tag/'+tab.url, function(){
+                $('#id_title').val(tab.title);
+                $('#id_desc' ).val();
+                // refresh the formset
+                $('.delete-row').remove();
+                $('.add-row').parent().remove();
+                acFormset(tag.repository+'/vocabularies/autocomplete', 'form');
             });
         }
-        else {
-            console.log(state);
-            // retrieve last state
-            body.html(state);
-            
-            $('#id_title').val(window.localStorage.getItem(id+'title'));
-            $('#id_notes').val(window.localStorage.getItem(id+'notes'));
-            $('#id_description').val(window.localStorage.getItem(id+'description'));
-            
-            tag.setAuth();
-            // refresh the formset
-            $('.delete-row').remove();
-            $('.add-row').parent().remove();
-            conceptFormset(tag.repository, 'form');
-        }
     },
-    
-    init: function(){
-        $('head').append("<link rel='stylesheet' href='" + tag.repository + "/css/autocomplete.css' />" +
-                         "<link rel='stylesheet' href='" + tag.repository + "/css/set.css' />");
 
+    init: function(){
         var port = chrome.extension.connect();
         chrome.tabs.getSelected(null, function(tab){
-            tag.tagging(tag.id(tab.url));
-        });
-        
-        $("button.send").on('click', function(){
-            $("button.send").text("Sending...");
+            tag.tagging(tab);
         });
 
         $("button.save").on('click', function(event){
             event.preventDefault();
             tag.saveCurrent();
-            $("button.save").animate({opacity: .5}, 500).animate({opacity:1}, 500);
+            $("button.save").animate({
+                opacity: .5}, 500).animate({
+                opacity: 1}, 500).parents('form:first').submit();
         });
 
         $("button.discard").on('click', function(event){
             event.preventDefault();
+            $(this).text("Crunching...");
             chrome.tabs.getSelected(null, function(tab){
-                window.localStorage.removeItem(tag.id(tab.url));
-            });
-            $("button.discard").text("Crunching...").animate({opacity:.5}, 500, function(){
-                location.reload();
+                tag.tagging(tab);
             });
         });
     }
