97f3455aca6e72de5a5c4cb71d866d8ec8893120
F. Malina <fmalina@pm.me>
2019-09-04 12:06:05 +0200

    Updated tagged record key

--- apps/tag/urls.py
+++ apps/tag/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('json/<int:record_id>', record_json, name="record_json"),
     path('query', query, name="query"),
     path('test', TaggingView.as_view(), name="test-record"),
-    path('-<path:uri>', TaggingView.as_view(), name="tag-record"),
-    path('<path:uri>', TaggingView.as_view(template_name='tag/record.html'), name="tag"),
+    path('-<path:key>', TaggingView.as_view(), name="tag-record"),
+    path('<path:key>', TaggingView.as_view(template_name='tag/record.html'), name="tag"),
 ]
