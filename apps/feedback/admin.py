dd607046887cab2409a77817bf0b60c00a7a3269
F. Malina <fmalina@pm.me>
2019-10-23 11:23:16 +0100

    User admin

--- apps/feedback/admin.py
+++ apps/feedback/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 from feedback.models import Feedback
 
 
@@ -9,48 +11,17 @@ def mark_done(modeladmin, request, queryset):
 mark_done.short_description = "Mark selected as done"
 
 
-def unmark_done(modeladmin, request, queryset):
-    queryset.update(done=False)
-
-
-unmark_done.short_description = "Selected are not done"
-
-
-def mark_awesome(modeladmin, request, queryset):
-    queryset.update(awesome=True)
-
-
-mark_awesome.short_description = "Mark selected as awesome"
-
-
-def unmark_awesome(modeladmin, request, queryset):
-    queryset.update(awesome=False)
-
-
-unmark_awesome.short_description = "Selected are not awesome"
-
-
-def mark_ignore(modeladmin, request, queryset):
-    queryset.update(ignore=True)
-
-
-mark_ignore.short_description = "Ignore selected"
-
-
-def unmark_ignore(modeladmin, request, queryset):
-    queryset.update(ignore=False)
-
-
-unmark_ignore.short_description = "Don't ignore selected"
-
-
 class FeedbackAdmin(admin.ModelAdmin):
     raw_id_fields = ('user',)
-    list_display = ('created_at', 'email', 'message',
-                    'url', 'done', 'awesome', 'ignore')
-    list_filter = ('created_at', 'done', 'awesome', 'ignore')
-    actions = [mark_done, unmark_done, mark_awesome,
-               unmark_awesome, mark_ignore, unmark_ignore]
+    list_display = ('message', 'email', 'url', 'created_at', 'done')
+    list_filter = ('created_at', 'done')
+    actions = [mark_done]
 
 
 admin.site.register(Feedback, FeedbackAdmin)
+
+
+UserAdmin.list_display = ('email', 'first_name', 'last_name', 'is_active', 'date_joined')
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
