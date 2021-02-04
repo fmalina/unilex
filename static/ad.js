4e483f8d3f14a67eb3f3ee0dffb98539013ab0c6
F. Malina <fmalina@pm.me>
2021-02-04 11:27:54 +0100

    Added one ad to each of unilex med product pages

---
+++ static/ad.js
@@ -0,0 +1,9 @@
+var s = document.createElement('script');
+s.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js';
+s.async = true;
+document.head.appendChild(s);
+
+(adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-7258089185308476",
+    enable_page_level_ads: true
+});
