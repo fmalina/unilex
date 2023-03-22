bb61ec44dbd311d741379858a706a511139e92a4
F. Malina <fmalina@pm.me>
2023-03-22 14:10:09 +0100

    Upgraded broken load_skos management command

--- unilex/vocabulary/management/commands/load_skos.py
+++ unilex/vocabulary/management/commands/load_skos.py
@@ -1,16 +1,16 @@
 from django.core.management.base import BaseCommand
 from unilex.vocabulary.load_skos import SKOSLoader
+from unilex.vocabulary.management.commands.dev_setup import create_user
 from optparse import make_option
-from .dev_setup import create_user
 
 
 class Command(BaseCommand):
     help = 'Import SKOS files'
     args = '<SKOS files>'
-    option_list = BaseCommand.option_list + (
+    option_list = [
         make_option('-r', dest='recursive', action='store_true',
             help='Import all vocabularies recursively.'),
-        )
+    ]
     
     def handle(self, *args, **options):   
         if not args:
@@ -25,11 +25,12 @@ class Command(BaseCommand):
                 for a in args:
                     loader.load_skos_vocab(a)
             loader.save_relationships()
-        except:
+        except Exception:
             import traceback, sys
             traceback.print_exc()
-    
-            print >>sys.stderr, '''
-                Exceptions have been raised.
+            print(
+                '''Exceptions have been raised.
                 If the import was incomplete, you might want to reset the DB:
-                $ python manage.py reset vocabulary'''
+                $ python manage.py reset vocabulary''',
+                file=sys.stderr
+            )
