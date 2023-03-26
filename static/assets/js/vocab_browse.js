d2f71c91ad113c875635a73bf86bef6587e8737c
F. Malina <fmalina@pm.me>
2023-03-26 21:10:50 +0200

    Enabled Content Security Policy

--- static/assets/js/vocab_browse.js
+++ static/assets/js/vocab_browse.js
@@ -1,16 +1,20 @@
-var VocabBrowser = {
+function Id(cssid){
+    return document.getElementById(cssid);
+}
+
+var VB = {
     root: null,
     toproot: null,
     init: function () {
-        $.get(location.pathname + 'json', VocabBrowser.initTree, 'json');
+        $.get(location.pathname + 'json', VB.initTree, 'json');
     },
     id: function(id) {
         return id.replace('v-', '');
     },
     initTree: function (json) {
-        VocabBrowser.root = json.id;
-        VocabBrowser.toproot = json.id;
-        var infovis = document.getElementById('infovis');
+        VB.root = json.id;
+        VB.toproot = json.id;
+        var infovis = Id('infovis');
         var w = infovis.offsetWidth, h = infovis.offsetHeight;
         w = h = Math.min(w, h);
         // init Hypertree
@@ -30,23 +34,23 @@ var VocabBrowser = {
             },
             // Attach event handlers and add text to the
             // labels. This method is only triggered on label creation
-            onCreateLabel: function (domElement, node){ // Change node styles when labels are placed or moved.
-                domElement.innerHTML = node.name;
+            onCreateLabel: function (el, node){ // Change node styles when labels are placed or moved.
+                el.innerHTML = node.name;
                 if(node.data.type == 'vocab'){
-                    $(domElement).addClass('vocabnode');
+                    el.classList.add('vocabnode');
                 }
                 if(json.queries){
                     if(node.data.query != ''){
-                        $(domElement).addClass('has_query');
+                        el.classList.add('has_query');
                     }
                 }
-                $(domElement).bind('click', function () { VocabBrowser.go(node.id); });
+                $(el).bind('click', function () { VB.go(node.id); });
             },
-            onPlaceLabel: function(domElement, node){
-                var style = domElement.style;
-                var current = node.id == VocabBrowser.root;
+            onPlaceLabel: function(el, node){
+                var style = el.style;
+                var current = node.id == VB.root;
                 for (var i = 0; i < 6; i++ ) {
-                    $(domElement).toggleClass('node-depth-' + i, node._depth == i);
+                    $(el).toggleClass('node-depth-' + i, node._depth == i);
                 }
                 style.display = '';
                 if (node.data.type == 'concept') {
@@ -55,29 +59,29 @@ var VocabBrowser = {
                     }
                 }
                 if (current) {
-                    VocabBrowser.showIcons(domElement, node);
+                    VB.showIcons(el, node);
                 } else {
-                    VocabBrowser.hideIcons(domElement, node);
+                    VB.hideIcons(el, node);
                 }
                 style.zIndex = 100 - node._depth;
                 var left = parseInt(style.left);
-                var w = domElement.offsetWidth;
-                var h = domElement.offsetHeight;
+                var w = el.offsetWidth;
+                var h = el.offsetHeight;
                 style.left = left - (w / 2) + 'px';
                 style.top = parseInt(style.top) - (h * 1.25) + 'px';
             },
             onAfterCompute: function(){
-                var node = VocabBrowser.rootNode();
+                var node = VB.rootNode();
                 if (node && node.id.indexOf("v-") == -1){
-                    $.get(VocabBrowser.conceptUri(node.id,'edit'), function (data) {
-                        $('#editing').html(data);
-                        // update page title
-                        var title = $('title').text().split(':')[0];
-                        $('title').text(title + ': ' + node.name);
+                    $.get(VB.conceptUri(node.id,'edit'), function (data) {
+                        Id('editing').innerHTML = data;
+                        document.title = document.title.split(':')[0] + ': ' + node.name;
+                        acFormset('/vocabularies/autocomplete',
+                                  document.querySelector('.set').id);
                     });
                 } else {
-                    $.get('/vocabularies/' + VocabBrowser.id(VocabBrowser.root) + '/edit', function (data) {
-                        $('#editing').html(data);
+                    $.get('/vocabularies/' + VB.id(VB.root) + '/edit', function (data) {
+                        Id('editing').innerHTML = data;
                     });
                 }
             }
@@ -87,34 +91,38 @@ var VocabBrowser = {
         // compute positions and plot.
         ht.refresh();
         ht.controller.onAfterCompute();
-        VocabBrowser.ht = ht;
-        setInterval(VocabBrowser.check_fragment, 100);
+        VB.ht = ht;
+        setInterval(VB.check_fragment, 100);
     },
     rootNode: function () {
-        if (VocabBrowser.ht) {
-            return VocabBrowser.ht.graph.getNode(VocabBrowser.root);
+        if (VB.ht) {
+            return VB.ht.graph.getNode(VB.root);
         }
     },
     go: function (id) {
-        if (VocabBrowser.root == id) return;
-        VocabBrowser.ht.onClick(id);
+        if (VB.root == id) return;
+        VB.ht.onClick(id);
         location.hash = 'c-' + id;
-        VocabBrowser.last_fragment = location.hash;
-        VocabBrowser.root = id;
+        VB.last_fragment = location.hash;
+        VB.root = id;
     },
     last_fragment: null,
     check_fragment: function () {
         var fragment = location.hash;
-        if (fragment != VocabBrowser.last_fragment) {
+        if (fragment != VB.last_fragment) {
             var mo = /c-([a-z0-9-]+)/.exec(fragment);
             if (mo)
-                VocabBrowser.go(mo[1]);
-            VocabBrowser.last_fragment = fragment;
+                VB.go(mo[1]);
+            VB.last_fragment = fragment;
             // redirect to right context when logging in
             // $('#loginlink').attr('href', this.href + location.hash);
         }
     },
-    showIcons: function (domElement, node) {
+    addBox: function (data) {
+        Id('action').innerHTML = data;
+        document.querySelector("#action #id_name").focus();
+    },
+    showIcons: function (el, node) {
         $('#editicons').remove();
         var del   = '<i class="icon-delete"  title="Delete"   id="icon-delete" ><b>×</b></i>';
         var cut   = '<i class="icon-cut"     title="Cut out"  id="icon-cut"    ></i>';
@@ -123,46 +131,40 @@ var VocabBrowser = {
         var edit  = '<i class="icon-edit"    title="Edit"     id="icon-edit"   ></i>';
         var save  = '<i class="icon-save"    title="Download" id="icon-save"   ></i>';
         if (node.data.type == 'vocab') {
-            $(domElement).append('<p id="editicons">' + save + add + del + paste + '</p>');
+            $(el).append('<p id="editicons">' + save + add + del + paste + '</p>');
             $('#icon-save').bind('click', function () {
                 location.href = location.pathname + 'skos'; 
             });
-            nid = VocabBrowser.id(node.id);
+            nid = VB.id(node.id);
             $('#icon-add').bind('click', function () {
-                $.get('/vocabularies/' + nid + '/new', function (data) {
-                    $('#editing').prepend(data);
-                    $("#editing input[type='text']:first").focus();
-                });
+                $.get('/vocabularies/' + nid + '/new', VB.addBox);
             });
             $('#icon-delete').bind('click', function () {
                 $.get('/vocabularies/' + nid + '/delete', function (data) {
-                    $('#editing').prepend(data);
+                    Id('action').innerHTML = data;
                 });
             });
             $('#icon-paste').bind('click', function (e) {
-                VocabBrowser.paste('direct_to_parent_vocab');
+                VB.paste('direct_to_parent_vocab');
                 e.preventDefault();
             });
         } else {
-            $(domElement).append('<p id="editicons">' + add + del + cut + paste + '</p>');
+            $(el).append('<p id="editicons">' + add + del + cut + paste + '</p>');
             $('#icon-add').bind('click', function () {
-                $.get(VocabBrowser.conceptUri(node.id,'new'), function (data) {
-                    $('#editing').prepend(data);
-                    $("#editing input[type='text']:first").focus();
-                });
+                $.get(VB.conceptUri(node.id, 'new'), VB.addBox);
             });
             $('#icon-delete').bind('click', function () {
-                $.get(VocabBrowser.conceptUri(node.id,'delete'), function (data) {
-                    $('#editing').prepend(data);
+                $.get(VB.conceptUri(node.id, 'delete'), function (data) {
+                    Id('action').innerHTML = data;
                 });
             });
             $('#icon-cut').bind('click', function () {
                 localStorage.removeItem("cut");
-                localStorage.setItem("cut", VocabBrowser.id(VocabBrowser.toproot) + ':' + node.id);
+                localStorage.setItem("cut", VB.id(VB.toproot) + ':' + node.id);
                 $(this).css('opacity', '0.3');
             });
             $('#icon-paste').bind('click', function (e) {
-                VocabBrowser.paste(node.id);
+                VB.paste(node.id);
                 e.preventDefault();
             });
         }
@@ -171,21 +173,21 @@ var VocabBrowser = {
     paste: function (nodeId) {
         $.post('/vocabularies/adopt', {
             child: localStorage.getItem("cut"),
-            parent: VocabBrowser.id(VocabBrowser.toproot) + ':' + nodeId
+            parent: VB.id(VB.toproot) + ':' + nodeId
         }).done(function() {
             location.reload();
         });
     },
-    hideIcons: function (domElement, node) {
-        $(domElement).html(node.name);
+    hideIcons: function (el, node) {
+        $(el).html(node.name);
     },
     conceptUri: function(nodeId, action) {
-        return '/vocabularies/' + VocabBrowser.id(VocabBrowser.toproot) + '/' + nodeId + '/' + action;
+        return '/vocabularies/' + VB.id(VB.toproot) + '/' + nodeId + '/' + action;
     },
 }
     
 $(function(){
-    VocabBrowser.init();
+    VB.init();
     $(document).on('click', '.cancel', function(){
         $(this).parents('form:first').remove();
     });
