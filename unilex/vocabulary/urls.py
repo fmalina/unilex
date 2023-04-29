78cb1441d2223eb7919ae2be3be6c7cde045132a
F. Malina <fmalina@pm.me>
2023-04-29 15:21:48 +0200

    Fixed minor URL bugs for enterprise subdirectory installs

--- unilex/vocabulary/urls.py
+++ unilex/vocabulary/urls.py
@@ -4,18 +4,18 @@ from unilex.vocabulary import views as v
 urlpatterns = [
     path('', v.listings, name='listings'),
     path('add', v.vocabulary_add, name='add'),
-    path('load-<slug:format>', v.load_vocab, name='load'),
+    path('load/<slug:format>', v.load_vocab, name='load'),
     path('authority/<slug:authority_code>/', v.authority, name='authority'),
     path('authority/<slug:authority_code>/json', v.authority, {'json': True}, name='authority_json'),
     path('authority/<slug:authority_code>/add', v.vocabulary_add, name='authority_add'),
     path('authority/<slug:authority_code>/load-<slug:format>', v.load_vocab, name='authority_load'),
     path('autocomplete', v.autocomplete, name='v-autocomplete'),
-
     path('generate', v.generate, name='generate'),
     path('search', v.search, name='search'),
+    path('adopt', v.concept_adopt),
 
     path('<slug:vocab_node_id>', v.detail, name='vocabulary'),
-    path('<slug:vocab_node_id>/', v.detail, name='vocabulary'),
+    path('<slug:vocab_node_id>/', v.detail),
     path('<slug:vocab_node_id>/edit', v.vocabulary_edit),
     path('<slug:vocab_node_id>/delete', v.vocabulary_delete),
     path('<slug:vocab_node_id>.json', v.json, name='json'),
@@ -28,5 +28,4 @@ urlpatterns = [
     re_path(r'^(?P<vocab_node_id>[a-z0-9-_]+)/(?P<node_id>[A-Za-z0-9-: ]+)/new', v.concept_new),
     re_path(r'^(?P<vocab_node_id>[a-z0-9-_]+)/(?P<node_id>[A-Za-z0-9-: ]+)/edit', v.concept_edit),
     re_path(r'^(?P<vocab_node_id>[a-z0-9-_]+)/(?P<node_id>[A-Za-z0-9-: ]+)/delete', v.concept_delete),
-    re_path('adopt', v.concept_adopt),
 ]
