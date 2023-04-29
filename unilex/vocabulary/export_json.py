39be78fd4bdd4bb3d4dbc49d4b2cbe197178c338
F. Malina <fmalina@pm.me>
2023-04-29 12:09:41 +0200

    Compute a fast children lookup dict to eliminate countless iterations

--- unilex/vocabulary/export_json.py
+++ unilex/vocabulary/export_json.py
@@ -2,14 +2,7 @@ from django.utils.html import escape
 from unilex.vocabulary.models import Concept
 
 
-def get_children(concept, vocab_concepts, parent_rels):
-    child_pks = [x.from_concept_id for x in parent_rels
-                 if x.to_concept_id == concept.id]
-    children = [x for x in vocab_concepts if x.id in child_pks]
-    return children
-
-
-def concept_to_dict(concept, vocab_concepts, parent_rels, depth, max_depth):
+def concept_to_dict(concept, children_lookup, depth, max_depth):
     d = {
         'id': concept.node_id,
         'name': escape(concept.name),
@@ -24,21 +17,25 @@ def concept_to_dict(concept, vocab_concepts, parent_rels, depth, max_depth):
     if not concept.active:
         d['data']['active'] = concept.active
     if depth < max_depth or max_depth == 0:
-        children = get_children(concept, vocab_concepts, parent_rels)
+        children = children_lookup.get(concept.id, [])
         if children:
             depth += 1
         else:
             depth -= 1
-        d['children'] = [concept_to_dict(child, vocab_concepts, parent_rels, depth, max_depth)
+        d['children'] = [concept_to_dict(child, children_lookup, depth, max_depth)
                          for child in children]
         d['data']['depth'] = depth
     return d
 
 
 def vocab_to_dict(vocab, max_depth=0):
-    vocab_concepts = list(vocab.concept_set.all())
-    pks = [c.pk for c in vocab_concepts]
-    parent_rels = list(Concept.parent.through.objects.filter(from_concept_id__in=pks))
+    vocab_concepts = {c.id: c for c in vocab.concept_set.all()}
+    parent_rels = Concept.parent.through.objects.filter(
+        from_concept_id__in=vocab_concepts.keys())
+    children_lookup = {}
+    for rel in parent_rels:
+        child = vocab_concepts[rel.from_concept_id]
+        children_lookup.setdefault(rel.to_concept_id, []).append(child)
 
     children = vocab.concept_set.filter(parent__isnull=True)
     # prepending 'v', vocabulary ID cannot match any concept id on the same page
@@ -49,7 +46,7 @@ def vocab_to_dict(vocab, max_depth=0):
         'url': vocab.get_absolute_url(),
         'queries': vocab.queries,
         'description': escape(vocab.description),
-        'children': [concept_to_dict(child, vocab_concepts, parent_rels, depth=1, max_depth=max_depth)
+        'children': [concept_to_dict(child, children_lookup, depth=1, max_depth=max_depth)
                      for child in children],
         'data': {'type': 'vocab'}
     }
