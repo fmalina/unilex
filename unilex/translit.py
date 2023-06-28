f5116e455b0f6baf2739b07b6af8c7042e735801
F. Malina <fmalina@pm.me>
2023-06-28 23:22:20 +0200

    Translit improved

--- unilex/translit.py
+++ unilex/translit.py
@@ -1,25 +1,43 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from transliterate import translit
-from transliterate.utils import LanguageDetectionError
 import requests
+from django.http import HttpResponse
+from django.shortcuts import render
+from lxml.html import fromstring, tostring
+from transliterate import translit, utils as err
 
 
-def handle404(request, exception):
-    return render(request, '404.html', {'exception': exception})
+def is_url(text):
+    if " " in text:
+        return False
+    if any(ord(char) > 127 for char in text):
+        return False
+    parts = text.split("/")
+    if len(parts) and "." not in parts[0]:
+        return False
+    return True
+
+
+def get_lang(dom):
+    try:
+        return dom.cssselect('html')[0].get('lang').split('-')[0]
+    except IndexError:
+        return
 
 
 def translit_view(request, s):
+    s = s.replace('http://', '').replace('https://', '')
+    url = is_url(s)
+    lang = None
+    if url:
+        u = f'http://{s}'
+        s = requests.get(u).content.decode()
+        dom = fromstring(s)
+        lang = get_lang(dom)
+        dom.make_links_absolute(u)
+        s = tostring(dom).decode()
     try:
-        if '.' in s and ' ' not in s:
-            url = f'https://{s}'
-            if url:
-                response = requests.get(url)
-                body = response.content.decode()
-                result = translit(body, reversed=True)
-        else:
-            result = translit(s, reversed=True)
-    except LanguageDetectionError:
-        return handle404(request, exception=None)
-
-    return HttpResponse(result)
+        result = translit(s, language_code=lang, reversed=True)
+    except (err.LanguageDetectionError, err.LanguagePackNotFound):
+        result = s
+    if url:
+        return HttpResponse(result, status=404)
+    return render(request, 'translit.html', {'content': result}, status=404)
